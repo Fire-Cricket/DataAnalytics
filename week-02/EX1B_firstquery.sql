acdc90679c1d0f323f949d9f311e6ad2eee208f8
Jordan Worrobah <jordanworrobah@gmail.com.com>
2026-04-14 21:53:05 -0400

    Added queries of actor and film tables

--- week-02/EX1B_firstquery.sql
+++ week-02/EX1B_firstquery.sql
@@ -1,3 +1,7 @@
 /*
 Sakila is a database used to collect innformation about DVDs and rental foruse buy consumers
 */
+
+USE sakila;
+SELECT * FROM actor; -- Retrieved 200 records
+SELECT * FROM film;
