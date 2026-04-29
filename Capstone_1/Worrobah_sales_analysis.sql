4fb61be5aeceb7d649b3c5737a359f0637a691e0
Jordan Worrobah <jordanworrobah@gmail.com.com>
2026-04-29 14:33:53 -0400

    Updating Cap stone folder and assignment

--- Capstone_1/Worrobah_sales_analysis.sql
+++ Capstone_1/Worrobah_sales_analysis.sql
@@ -1,4 +1,3 @@
--- Jordan Worrobah Sales Analysis
 -- Territory: See Ellefson
 
 USE sample_sales;
@@ -37,6 +36,53 @@ WHERE m.SalesManager = 'See Ellefson'
 GROUP BY month
 ORDER BY month;
 
-SELECT m.Region,
-	m.SalesManager,
-    SUM(SS.Sales_Amount) AS total_revenue
+SELECT 
+    m.Region,
+    m.SalesManager,
+    SUM(ss.Sale_Amount) AS 'Total Revenue'
+FROM store_sales ss
+JOIN store_locations sl 
+    ON ss.Store_ID = sl.StoreId
+JOIN management m 
+    ON sl.State = m.State
+WHERE m.Region = 'East'
+GROUP BY m.Region, m.SalesManager
+ORDER BY total_revenue DESC;
+
+SELECT 
+    DATE_FORMAT(ss.Transaction_Date, '%Y-%m') AS month,
+    ic.Category,
+    COUNT(*) AS transactions,
+    ROUND(AVG(ss.Sale_Amount), 2) AS 'Avg Transaction'
+FROM store_sales ss
+JOIN products p 
+    ON ss.Prod_Num = p.Prod_Num
+JOIN inventory_subategories isc 
+    ON p.SubCategoryID = isc.SubCategoryID
+JOIN inventory_categories ic 
+    ON isc.CategoryID = ic.CategoryID
+JOIN store_locations sl 
+    ON ss.Store_ID = sl.StoreId
+JOIN management m 
+    ON sl.State = m.State
+WHERE m.SalesManager = 'See Ellefson'
+GROUP BY month, ic.Category
+ORDER BY month, ic.Category;
+
+SELECT 
+    sl.StoreLocation,
+    SUM(ss.Sale_Amount) AS 'Total Revenue'
+FROM store_sales ss
+JOIN store_locations sl 
+    ON ss.Store_ID = sl.StoreId
+JOIN management m 
+    ON sl.State = m.State
+WHERE m.SalesManager = 'See Ellefson'
+GROUP BY sl.StoreLocation
+ORDER BY 'Total Revenue' DESC;
+
+-- My recommendation for this comapny is teh stores and categories that are bringing the most money should be priortized next quarter.
+-- If they use most of their income to increase inventory and market stores that are performing the best they will defitly maximize their profit.
+-- Also,  categories with higher average transaction values should receive more focus. For lower performing 
+-- stores or categories targeted promotions may help increase their performance.
+ 
