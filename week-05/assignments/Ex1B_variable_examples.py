8bbfad64ae7fbbc81a7e680045d493809e89a65f
Jordan Worrobah <jordanworrobah@gmail.com.com>
2026-05-06 13:15:20 -0400

    Save Python practice changes

--- week-05/assignments/Ex1B_variable_examples.py
+++ week-05/assignments/Ex1B_variable_examples.py
@@ -1,3 +1,31 @@
-hello = 45
+Customer_ID = 123456
+Customers_name = 'Jordan Worrobah' # I wrote this because I knew I needed this as a string
+Customer_gender = 'Male'
+Customer_dob = '2/4/2005'
+Drivers_license_num = 98765
+Auto_policy_num = 56789
 
-print(hello)
+myName = 'Jordan R. Worrobah'
+myCity = 'West Philadelphia'
+myState = 'Pennsylvania'
+
+# a)
+'''
+False, def, if, raise
+None, del, import, return
+True, elif, in, try
+and, else, is, while
+as, except, lambda, with
+assert, finally, nonlocal, yield
+break, for, not
+class, form, or
+'''
+
+# b)
+'''
+False is used
+def is used to define a function
+if is used to create an if statement
+import is used to import a libray
+return is used to return a statement 
+'''
